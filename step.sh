7009a4bcba523b485ef71f48dcd74c62ec772a42
Kent Boogaart <kent.boogaart@gmail.com>
2017-05-29 10:41:37 +0930

    Handle fallback when no channel or version set

--- step.sh
+++ step.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -ex
+
 echo "Installing .NET core"
 echo "--------------------"
 echo ""
@@ -25,6 +28,9 @@ elif [ -n "${version}" ]
   then
     echo "Installing version: ${version}"
     ./dotnet-install.sh  --version "${version}"
+else
+    echo "Installing default channel and version"
+    ./dotnet-install.sh
 fi
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
